ebcf6f7afd90e3bb62d148c59f34fcbbd450abd5
Nicola Ferraro <nibbio84@gmail.com>
2015-04-25 16:38:38 +0200

    Fix shell

--- hbase-shell-0.98.10.1/start-shell.sh
+++ hbase-shell-0.98.10.1/start-shell.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-/opt/hbase/bin/hbase/start-pseudo-distributed.sh > logmaster.log 2>&1 &
+/opt/hbase/bin/start-pseudo-distributed.sh > logmaster.log 2>&1 &
 
 /opt/hbase/bin/hbase shell
