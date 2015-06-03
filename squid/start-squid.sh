798db9932bb0f6023eba62837b567ab9fe99bec7
Nicola Ferraro <nibbio84@gmail.com>
2015-06-03 22:59:09 +0200

    Added extra to Squid

--- squid/start-squid.sh
+++ squid/start-squid.sh
@@ -5,6 +5,7 @@ cp squid.conf /etc/squid/
 echo "cache_peer $proxy_host parent $proxy_port 0 no-query default login=$proxy_user:$proxy_pass" >> /etc/squid/squid.conf
 echo "never_direct allow all" >> /etc/squid/squid.conf
 echo "http_port $listen" >> /etc/squid/squid.conf
+echo "$extra" >> /etc/squid/squid.conf
 
 service squid start
 
