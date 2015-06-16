b65aaafb4682e2f57c405e20a737efd2176eac54
Nicola Ferraro <nibbio84@gmail.com>
2015-06-17 00:49:44 +0200

    Dockers for HBase 1.1.0.1, Phoenix 4.4.0, fix

--- hbase-phoenix-1.1.0.1-4.4.0/install-phoenix.sh
+++ hbase-phoenix-1.1.0.1-4.4.0/install-phoenix.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-wget http://archive.eu.apache.org/dist/phoenix/phoenix-4.4.0-HBase-1.1/bin/phoenix-4.4.0-HBase-1.1-bin.tar.gz
 tar -xzf phoenix-4.4.0-HBase-1.1-bin.tar.gz
 mv phoenix-4.4.0-HBase-1.1-bin /opt/
 rm -f phoenix-4.4.0-HBase-1.1-bin.tar.gz
 ln -s /opt/phoenix-4.4.0-HBase-1.1-bin /opt/phoenix
 
-cp /opt/phoenix/phoenix-4.4.0-server.jar /opt/hbase/lib/
+cp /opt/phoenix/phoenix-server-4.4.0-HBase-1.1.jar /opt/hbase/lib/
