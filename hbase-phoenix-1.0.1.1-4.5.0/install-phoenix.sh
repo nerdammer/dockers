c709a00f339ee54e69698d27984ca2975702fa4c
Nicola Ferraro <nibbio84@gmail.com>
2015-07-31 09:27:03 +0200

    HBase-Phoenix 1.0.1.1-4.5.0

---
+++ hbase-phoenix-1.0.1.1-4.5.0/install-phoenix.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+tar -xzf phoenix-4.5.0-HBase-1.0-bin.tar.gz
+mv phoenix-4.5.0-HBase-1.0-bin /opt/
+rm -f phoenix-4.5.0-HBase-1.0-bin.tar.gz
+ln -s /opt/phoenix-4.5.0-HBase-1.0-bin /opt/phoenix
+
+cp /opt/phoenix/phoenix-server-4.5.0-HBase-1.0.jar /opt/hbase/lib/
+cp /opt/phoenix/phoenix-core-4.5.0-HBase-1.0.jar /opt/hbase/lib/
